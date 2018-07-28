61c722fc54bc36e2f191445e0b7865259bfc1d00
vikaspulluri <vikasiiitn@gmail.com>
2018-07-29 01:52:19 +0530

    Updated comments

--- Youtube.js
+++ Youtube.js
@@ -1,3 +1,63 @@
+/**
+ * @author: Vikas Pulluri
+ * @date: 29/07/2018
+ * @file: Youtube.ts
+ * @description: This is a simple youtube look alike crud oriented backend code.
+ *              It uses multiple classes and each class serves separate purpose
+ */
+/**
+ * @description: This is basic data transfer object for every video. It contains all the basic
+ *              properties and simple light weight functions associated with video. This is the
+ *              object that carries the data between multiple processes.
+ */
+var VideoDTO = /** @class */ (function () {
+    function VideoDTO(builder) {
+        this.numberOfViews = 0;
+        this.likes = 0;
+        this.dislikes = 0;
+        this.title = builder._name;
+        this._id = builder.ID;
+        this.videoUrl = builder._videoUrl;
+        this.videoCategory = builder._category;
+        this.videoThumbnailsUrl = builder._thumbnails || [];
+        this.isCensoredConent = builder._isCensored || false;
+        this.uploader = builder._uploader;
+        this.likes = builder.getLikes;
+        this.dislikes = builder.getDislikes;
+        this.numberOfViews = builder.getViews;
+    }
+    ;
+    VideoDTO.prototype.toString = function () {
+        return JSON.stringify(this);
+    };
+    VideoDTO.prototype.reOrder = function () {
+        var obj = {};
+        var self = this;
+        for (var key in this) {
+            if (this.hasOwnProperty(key) && key !== this._id) {
+                obj[key] = this[key];
+            }
+        }
+        var obj2 = {};
+        obj2[this._id] = obj;
+        return obj2;
+    };
+    VideoDTO.prototype.updateAuthor = function (author, obj) {
+        obj.author = author;
+        return obj;
+    };
+    VideoDTO.prototype.updateTitle = function (title, obj) {
+        obj.title = title;
+        return obj;
+    };
+    return VideoDTO;
+}());
+/**
+ * @description: This class is responsible for building/constructing the video based on the given data
+ *              and returns a new instance of Data transfer object(DTO). It implements the "Builder"
+ *              design pattern in order to build the video based on the given data.
+ *
+ */
 var VideoBuilder = /** @class */ (function () {
     function VideoBuilder(_id) {
         this._id = _id;
@@ -81,52 +141,16 @@ var VideoBuilder = /** @class */ (function () {
         return this;
     };
     VideoBuilder.prototype.build = function () {
-        return new YoutubeDTO(this);
+        return new VideoDTO(this);
     };
     return VideoBuilder;
 }());
-var YoutubeDTO = /** @class */ (function () {
-    function YoutubeDTO(builder) {
-        this.numberOfViews = 0;
-        this.likes = 0;
-        this.dislikes = 0;
-        this.title = builder._name;
-        this._id = builder.ID;
-        this.videoUrl = builder._videoUrl;
-        this.videoCategory = builder._category;
-        this.videoThumbnailsUrl = builder._thumbnails || [];
-        this.isCensoredConent = builder._isCensored || false;
-        this.uploader = builder._uploader;
-        this.likes = builder.getLikes;
-        this.dislikes = builder.getDislikes;
-        this.numberOfViews = builder.getViews;
-    }
-    ;
-    YoutubeDTO.prototype.toString = function () {
-        return JSON.stringify(this);
-    };
-    YoutubeDTO.prototype.reOrder = function () {
-        var obj = {};
-        var self = this;
-        for (var key in this) {
-            if (this.hasOwnProperty(key) && key !== this._id) {
-                obj[key] = this[key];
-            }
-        }
-        var obj2 = {};
-        obj2[this._id] = obj;
-        return obj2;
-    };
-    YoutubeDTO.prototype.updateAuthor = function (author, obj) {
-        obj.author = author;
-        return obj;
-    };
-    YoutubeDTO.prototype.updateTitle = function (title, obj) {
-        obj.title = title;
-        return obj;
-    };
-    return YoutubeDTO;
-}());
+/*
+@description: 'Youtuve' class can keep track of all videos, creating, reading, updating, deleting
+                individual video. This class works like a Youtube videos database and serves the similar
+                kind of functionality
+@methods: createVideo(), getVideos(), getVideoDetailsById(), getVideoDetailsByTitle(), updateVideoById(), deleteVideoById()
+*/
 var Youtube = /** @class */ (function () {
     function Youtube() {
         this.videosList = [];
@@ -136,9 +160,9 @@ var Youtube = /** @class */ (function () {
         return this.videosList;
     };
     Youtube.prototype.getVideos = function () {
-        for (var i = 1; i < this.videosList.length + 1; i++) {
-            console.log("Video - " + i);
-            console.log(this.videosList[i - 1]);
+        for (var _i = 0, _a = this.videosList; _i < _a.length; _i++) {
+            var video = _a[_i];
+            console.log(objectToString(video));
         }
         return this.videosList;
     };
@@ -146,6 +170,7 @@ var Youtube = /** @class */ (function () {
         for (var _i = 0, _a = this.videosList; _i < _a.length; _i++) {
             var video = _a[_i];
             if (Object.keys(video)[0] === String(id)) {
+                console.log(objectToString(video));
                 return video;
             }
         }
@@ -185,6 +210,10 @@ var Youtube = /** @class */ (function () {
     };
     return Youtube;
 }());
+function objectToString(obj) {
+    return JSON.stringify(obj);
+}
+//Constructing VideoDTO by using VideoBuilder. Created 3 VideoDTO's
 var video1 = new VideoBuilder(1001)
     .title("Thor")
     .categoryType("Action")
@@ -193,6 +222,8 @@ var video1 = new VideoBuilder(1001)
     .dislikes(1500)
     .views(5711846)
     .author('Vikas')
+    .video('https://www.youtube.com/watch?v=93dC0o2aHto')
+    .videoThumbnails('https://www.gstatic.com/webp/gallery/1.sm.jpg', 'https://www.gstatic.com/webp/gallery/2.sm.jpg')
     .build()
     .reOrder();
 var video2 = new VideoBuilder(1002)
@@ -201,6 +232,7 @@ var video2 = new VideoBuilder(1002)
     .censored(false)
     .likes(45587744)
     .dislikes(1554)
+    .author('Ganesh')
     .views(7884477946)
     .build()
     .reOrder();
@@ -210,27 +242,29 @@ var video3 = new VideoBuilder(1003)
     .censored(false)
     .likes(4558774)
     .dislikes(154)
+    .author('Sahit')
     .views(78877946)
     .build()
     .reOrder();
+//creating a actual instance of youtube class which hold all info about all videos
 var youtube = new Youtube();
-console.log('Initial videos:');
-//Creating videos
+console.log('Initially we have 2 videos:');
+//Adding above videos to main youtube instance
 youtube.createVideo(video1);
 youtube.createVideo(video2);
 youtube.getVideos();
-console.log('After adding new videos:');
+console.log('After adding new video:');
 youtube.createVideo(video3);
-//Get videos
+//Get all videos
 youtube.getVideos();
 //Update videos
-console.log("After updation: ");
+console.log("After updating the title and author of video with id 1001: ");
 youtube.updateVideoById(1001, { title: "Thor-2", uploader: "Vikas Pulluri" });
-console.log(video1);
+youtube.getVideos();
 //Delete videos
-youtube.deleteVideoById(1003);
-console.log('After Deletion: ');
+youtube.deleteVideoById(1002);
+console.log('After deletion of one video: ');
 youtube.getVideos();
 //Get videos by ID
 console.log('Getting videos by id');
-console.log(youtube.getVideoDetailsById(1002));
+youtube.getVideoDetailsById(1003);
--- Youtube.ts
+++ Youtube.ts
@@ -1,3 +1,75 @@
+/**
+ * @author: Vikas Pulluri
+ * @date: 29/07/2018
+ * @file: Youtube.ts
+ * @description: This is a simple youtube look alike crud oriented backend code.
+ *              It uses multiple classes and each class serves separate purpose 
+ */
+
+
+
+/**
+ * @description: This is basic data transfer object for every video. It contains all the basic
+ *              properties and simple light weight functions associated with video. This is the 
+ *              object that carries the data between multiple processes.
+ */
+class VideoDTO {
+    public title:string; 
+    public videoUrl:string;
+    public videoCategory:string;
+    public videoThumbnailsUrl:string[];
+    public isCensoredConent:boolean;
+    public numberOfViews:number = 0;;
+    public likes:number = 0;
+    public dislikes:number = 0;
+    public uploader:string ;
+    public _id:number;
+    constructor(builder:VideoBuilder){
+        this.title = builder._name;
+        this._id = builder.ID;
+        this.videoUrl = builder._videoUrl;
+        this.videoCategory = builder._category;
+        this.videoThumbnailsUrl = builder._thumbnails || [];
+        this.isCensoredConent = builder._isCensored || false;
+        this.uploader = builder._uploader;
+        this.likes = builder.getLikes;
+        this.dislikes = builder.getDislikes;
+        this.numberOfViews = builder.getViews;
+    }
+
+    toString():string{
+        return JSON.stringify(this);
+    }
+
+    reOrder():object{
+        let obj: {[k: string]: any} = {};
+        let self = this;
+        for(let key in this){
+            if(this.hasOwnProperty(key) && key !== this._id){
+                obj[key] = this[key];
+            }
+        }
+        let obj2: {[k: number]: any} = {};
+        obj2[this._id] = obj;
+        return obj2;
+    }
+    updateAuthor(author:string,obj:{[k:string]:any}):object{
+        obj.author = author;
+        return obj;
+    }
+    updateTitle(title:string,obj:{[k:string]:any}):object{
+        obj.title = title;
+        return obj;
+    }
+    
+}
+
+/**
+ * @description: This class is responsible for building/constructing the video based on the given data
+ *              and returns a new instance of Data transfer object(DTO). It implements the "Builder" 
+ *              design pattern in order to build the video based on the given data.
+ *               
+ */
 class VideoBuilder{
     public _name:string = "";
     public _videoUrl:string = "";
@@ -70,62 +142,17 @@ class VideoBuilder{
         this._dislikes = dislikes;
         return this;
     }
-    build():YoutubeDTO{
-        return new YoutubeDTO(this);
+    build():VideoDTO{
+        return new VideoDTO(this);
     }
 }
 
-class YoutubeDTO {
-    public title:string; 
-    public videoUrl:string;
-    public videoCategory:string;
-    public videoThumbnailsUrl:string[];
-    public isCensoredConent:boolean;
-    public numberOfViews:number = 0;;
-    public likes:number = 0;
-    public dislikes:number = 0;
-    public uploader:string ;
-    public _id:number;
-    constructor(builder:VideoBuilder){
-        this.title = builder._name;
-        this._id = builder.ID;
-        this.videoUrl = builder._videoUrl;
-        this.videoCategory = builder._category;
-        this.videoThumbnailsUrl = builder._thumbnails || [];
-        this.isCensoredConent = builder._isCensored || false;
-        this.uploader = builder._uploader;
-        this.likes = builder.getLikes;
-        this.dislikes = builder.getDislikes;
-        this.numberOfViews = builder.getViews;
-    }
-
-    toString():string{
-        return JSON.stringify(this);
-    }
-
-    reOrder():object{
-        let obj: {[k: string]: any} = {};
-        let self = this;
-        for(let key in this){
-            if(this.hasOwnProperty(key) && key !== this._id){
-                obj[key] = this[key];
-            }
-        }
-        let obj2: {[k: number]: any} = {};
-        obj2[this._id] = obj;
-        return obj2;
-    }
-    updateAuthor(author:string,obj:{[k:string]:any}):object{
-        obj.author = author;
-        return obj;
-    }
-    updateTitle(title:string,obj:{[k:string]:any}):object{
-        obj.title = title;
-        return obj;
-    }
-    
-}
-
+/*
+@description: 'Youtuve' class can keep track of all videos, creating, reading, updating, deleting
+                individual video. This class works like a Youtube videos database and serves the similar
+                kind of functionality
+@methods: createVideo(), getVideos(), getVideoDetailsById(), getVideoDetailsByTitle(), updateVideoById(), deleteVideoById()
+*/
 class Youtube{
     protected videosList:object[] = [];
     constructor(){}
@@ -136,9 +163,8 @@ class Youtube{
     }
 
     getVideos(){
-        for(let i=1;i<this.videosList.length+1;i++){
-            console.log(`Video - ${i}`);
-            console.log(this.videosList[i-1]);
+        for(let video of this.videosList){
+            console.log(objectToString(video));
         }
         return this.videosList;
     }
@@ -146,6 +172,7 @@ class Youtube{
     getVideoDetailsById(id:number):object{
         for(let video of this.videosList){
             if(Object.keys(video)[0] === String(id)){
+                console.log(objectToString(video));
                 return video;
             }
         }
@@ -185,6 +212,11 @@ class Youtube{
     }
 }
 
+function objectToString(obj:object):string{
+    return JSON.stringify(obj);
+}
+
+//Constructing VideoDTO by using VideoBuilder. Created 3 VideoDTO's
 let video1 = new VideoBuilder(1001)
                         .title("Thor")
                         .categoryType("Action")
@@ -193,6 +225,8 @@ let video1 = new VideoBuilder(1001)
                         .dislikes(1500)
                         .views(5711846)
                         .author('Vikas')
+                        .video('https://www.youtube.com/watch?v=93dC0o2aHto')
+                        .videoThumbnails('https://www.gstatic.com/webp/gallery/1.sm.jpg','https://www.gstatic.com/webp/gallery/2.sm.jpg')
                         .build()
                         .reOrder();
 let video2 = new VideoBuilder(1002)
@@ -201,6 +235,7 @@ let video2 = new VideoBuilder(1002)
                         .censored(false)
                         .likes(45587744)
                         .dislikes(1554)
+                        .author('Ganesh')
                         .views(7884477946)
                         .build()
                         .reOrder();
@@ -210,36 +245,39 @@ let video3 = new VideoBuilder(1003)
                         .censored(false)
                         .likes(4558774)
                         .dislikes(154)
+                        .author('Sahit')
                         .views(78877946)
                         .build()
                         .reOrder();
+//creating a actual instance of youtube class which hold all info about all videos
 const youtube = new Youtube();
 
-console.log('Initial videos:');
+console.log('Initially we have 2 videos:');
 
-//Creating videos
+//Adding above videos to main youtube instance
 youtube.createVideo(video1);
 youtube.createVideo(video2);
+
 youtube.getVideos();
 
-console.log('After adding new videos:');
+console.log('After adding new video:');
 youtube.createVideo(video3);
 
-//Get videos
+//Get all videos
 youtube.getVideos();
 
 
 //Update videos
-console.log("After updation: ")
+console.log("After updating the title and author of video with id 1001: ")
 youtube.updateVideoById(1001,{title:"Thor-2",uploader:"Vikas Pulluri"});
 
-console.log(video1);
+youtube.getVideos();
 
 //Delete videos
-youtube.deleteVideoById(1003);
-console.log('After Deletion: ');
+youtube.deleteVideoById(1002);
+console.log('After deletion of one video: ');
 youtube.getVideos();
 
 //Get videos by ID
 console.log('Getting videos by id');
-console.log(youtube.getVideoDetailsById(1002));
+youtube.getVideoDetailsById(1003);
